50827af87b7d64ab7be3befe60e09ac28d62f294
Nathalia Castelo Branco <nathaliaacbranco@gmail.com>
2021-10-22 11:43:10 -0300

    initial project version

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from main import get_prediction
+
+st.image('image.jpg', caption='Photo by @artbyhybrid from Unsplash')
+st.title('Análise de sentimentos com uso do algoritmo BERT\n')
+st.write('O objetivo deste projeto é identificar se uma frase em inglês representa um sentimento positivo ou negativo. O modelo possui acurácia de 82%.')
+
+user_input = st.text_input('Insira uma frase em inglês:')
+
+prediction_btn = st.button('Enviar', help='Descubra se sua frase é positiva ou negativa')
+# , on_click=get_prediction(user_input)
+
+if prediction_btn or user_input:
+    prediction = get_prediction(user_input)
+
+    if prediction == 0:
+        emotion = 'Negative'
+        text = 'Oh, não! Sua frase é negativa. Está tudo bem? :cry:'
+    elif prediction == 1:
+        emotion = 'Positive'
+        text = 'Viva! Sua frase é positiva. :smile:'
+
+    st.write(text)
+
---
+++ main.py
@@ -0,0 +1,48 @@
+# Required libraries
+import math
+import re
+import tensorflow as tf 
+import tensorflow_hub as hub 
+import bert
+from tensorflow.keras.models import load_model
+
+# Tokenization phase
+'''
+    1º - Create tokenizer
+    2º - Create bert layer based on BERT LARGE
+    3º - Define the location of the vocabulary file
+    4º - Lower all word cases
+    5º - Define instance to tokenize and lower cases
+'''
+FullTokenizer = bert.bert_tokenization.FullTokenizer 
+bert_layer = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/4', trainable=False)
+vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy() 
+do_lower_case = bert_layer.resolved_object.do_lower_case.numpy() 
+tokenizer = FullTokenizer(vocab_file, do_lower_case)
+
+# Create encoder function
+'''
+    This function converts tokens to ids
+'''
+def encode_sentence(sent):
+  return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sent))
+
+
+# Restore the trained model
+Dcnn_trained = tf.keras.models.load_model('saved_model\sentiment-analysis-bert')
+
+# Prediction function
+def get_prediction(sentence):
+  tokens = encode_sentence(sentence)
+  inputs = tf.expand_dims(tokens, 0) 
+
+  output = Dcnn_trained(inputs, training=False) 
+
+  sentiment = math.floor(output*2)
+
+  if sentiment == 0:
+    print('Oh, não! Sua frase é negativa. Está tudo bem? :cry:')
+  elif sentiment == 1:
+    print('Viva! Sua frase é positiva. :smile:')
+
+  return sentiment
